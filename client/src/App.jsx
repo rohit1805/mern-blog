6d0fe55f9ffc9a4769bbe8db3a95307d5f751f77
Rohit Nandavdekar <rohitnandavdekar007@gmail.com>
2024-06-16 10:48:15 +0530

    Added pages and created routes in react app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,27 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+import Project from "./pages/Project";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 function App() {
-  return <div className="text-4xl">Blog</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
